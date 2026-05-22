6dfd7209b58223dd7fb5498dd691c74299ecd5a0
tiboryeah <esteban_rock@outlook.es>
2026-05-21 18:11:55 -0600

    fix(header): logo mas grande h-9 -> h-14

--- src/pages/public/header/headerPublic.tsx
+++ src/pages/public/header/headerPublic.tsx
@@ -100,7 +100,7 @@ export default function Header() {
             <img
               src={logoImg}
               alt="FABRIC"
-              className="h-9 w-auto object-contain opacity-90 group-hover:opacity-100 transition-opacity duration-500"
+              className="h-14 w-auto object-contain opacity-90 group-hover:opacity-100 transition-opacity duration-500"
             />
           </Link>
 
